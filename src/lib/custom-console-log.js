cd3e56cb50ccda2f5d127bb7cd1e34563c7b3b87
David-Vandensteen <dvandensteen@gmail.com>
2023-10-29 16:45:42 +0100

    Update : custom console log

--- src/lib/custom-console-log.js
+++ src/lib/custom-console-log.js
@@ -2,7 +2,6 @@ import chalk from 'chalk';
 
 const { log, error } = console;
 
-log.title = (...message) => { log(chalk.bgGreen.bold('-', ...message)); };
 log.info = (...message) => { log(chalk.magenta.bold('  .', ...message)); };
 log.debug = (...message) => { log(...message); };
 log.error = (...message) => { error(...message); };
