6896b39aba5c3f008c8bd1063a6c888497371beb
David-Vandensteen <dvandensteen@gmail.com>
2023-11-09 19:06:54 +0100

    Remove : useless code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// import { ApplicationService } from '#src/service/application';
-// import YamlLoader from '#src/lib/yaml-loader';
-// import { log } from 'custom-console-log';
-
-// const configFile = './src/config/default.yaml';
-
-// const config = YamlLoader(configFile);
-
-// if (config) log.green('Config', configFile, 'was loaded');
-
-// new ApplicationService(config.port, config.mapping, {
-//   midiIn: 'Gestionnaire IAC Bus 1',
-//   midiOut: 'Gestionnaire IAC Bus 1',
-// }) // TODO :
-//   .midiConnect()
-//   .serve();
-
 import { ApplicationService } from '#src/service/application';
 
 export default ApplicationService;
