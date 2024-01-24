d69b46776e010ffe448203cf3a181d2163d7ea2d
David-Vandensteen <dvandensteen@gmail.com>
2024-01-24 18:13:05 +0100

    improve config parser

--- src/service/config_loader.js
+++ src/service/config_loader.js
@@ -1,15 +1,12 @@
 import YamlLoader from '#src/lib/yaml_loader';
 import { ConfigModel } from '#src/model/config';
+import { MidiModel } from '#src/model/midi';
+import { NavigationSceneModel } from '#src/model/navigation/scene';
+import { SceneModel } from '#src/model/scene';
+import { MappingModel } from '#src/model/mapping';
 import { log } from 'custom-console-log';
 
-import {
-  ConfigError,
-  GlobalError,
-  NavigationError,
-  SceneError,
-  MappingError,
-  TypeError,
-} from '#src/model/error';
+import { ConfigError } from '#src/model/error';
 
 export default class ConfigLoaderService {
   #config;
@@ -21,26 +18,45 @@ export default class ConfigLoaderService {
     this.#parse();
   }
 
-  #parse() {
-    try {
-      (() => new ConfigModel(
-        this.#config.midi,
-        {
-          port: this.#config.port,
-          sceneNavigation: this.#config.sceneNavigation,
-          global: this.#config.global,
-          scenes: this.#config.scenes,
-        },
-      ))();
-    } catch (err) {
-      if (err instanceof SceneError) throw new SceneError(err);
-      else if (err instanceof GlobalError) throw new GlobalError(err);
-      else if (err instanceof NavigationError) throw new NavigationError(err);
-      else if (err instanceof MappingError) throw new MappingError(err);
-      else if (err instanceof TypeError) throw new TypeError(err);
+  #load() { // TODO
+    log('config load');
+    ConfigModel.deserialize(this.#config);
+  }
 
-      throw new ConfigError(err);
+  #parse() {
+    MidiModel.deserialize(this.#config.midi);
+
+    if (this.#config?.navigation?.scenes) {
+      this.#config.navigation.scenes.forEach((scene) => {
+        log('parse scene navigation', scene.id);
+        NavigationSceneModel.deserialize(scene);
+      });
     }
+
+    if (this.#config?.global?.mappings) {
+      this.#config.global.mappings.forEach((mapping) => {
+        log('parse global mapping', mapping.sequence); // TODO UNICODE sanitizer
+        MappingModel.deserialize(mapping);
+      });
+    }
+
+    if (this.#config?.scenes) {
+      this.#config.scenes.forEach((scene) => {
+        scene.mappings.forEach((mapping) => {
+          log('parse scene', scene.id, 'mapping', mapping.sequence);
+          MappingModel.deserialize(mapping);
+        });
+        log('parse scene', scene.id);
+        SceneModel.deserialize(scene);
+      });
+    }
+
+    log('parse config', this.#config.name);
+    ConfigModel.deserialize(this.#config);
+
+    log('');
+    log.green('config :', this.#config.name, 'is compliant');
+    log('');
   }
 
   get() { return this.#config; }
